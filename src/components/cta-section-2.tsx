5beb2219bed830b9a02d2a1fe003e8f476550706
Tessa Kriesel <me@tessak22.com>
2024-10-26 15:47:55 -0500

    remove rounded class and adjust iframe div classes

--- src/components/cta-section-2.tsx
+++ src/components/cta-section-2.tsx
@@ -3,14 +3,14 @@ export function CtaSection2() {
 
 	return (
 		<section className='container'>
-			<div className='flex flex-col items-center gap-6 rounded-xl px-6 sm:gap-10 bg-accent py-16'>
+			<div className='flex flex-col items-center gap-6 px-6 sm:gap-10 bg-accent py-16'>
 				<h3 className='font-bold uppercase text-background'>
 					Wanna know the industry secrets?
 				</h3>
 				<h2 className="font-heading text-3xl font-semibold tracking-tight sm:text-4xl text-balance sm:leading-tight text-center text-primary-foreground max-w-2xl">Stay Sharp on Developer Growth</h2>
 				<p className='max-w-xl text-lg text-primary-foreground/80 text-center'>Join the newsletter and receive content on developer tool traction in your inbox.</p>
 				<div
-					className='flex w-full content-center w-8/12'
+					className='flex items-center w-8/12'
 					dangerouslySetInnerHTML={{ __html: beehiveForm }}
 				/>
 			</div>
